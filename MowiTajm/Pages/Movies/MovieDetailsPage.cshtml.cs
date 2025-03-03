c4ceebc4b3587461700fedfd8c6a9de2188bc235
audioken <kenzachrisson@gmail.com>
2025-03-03 17:26:29 +0100

    la till nullkontroller och gjorde så datum skrivs ut för recensioner

--- MowiTajm/Pages/Movies/MovieDetailsPage.cshtml.cs
+++ MowiTajm/Pages/Movies/MovieDetailsPage.cshtml.cs
@@ -22,27 +22,31 @@ namespace MowiTajm.Pages.Movies
 
         [BindProperty]
         public Review Review { get; set; } = new();
-
         public List<Review> Reviews { get; set; } = new();
 
         public async Task OnGetAsync(string imdbId)
         {
-            Movie = await _omdbService.GetMovieByIdAsync(imdbId);
-            Reviews = await _database.Reviews.Where(r => r.ImdbID == imdbId).ToListAsync();
-            Review.ImdbID = imdbId;
+            if (!string.IsNullOrWhiteSpace(imdbId))
+            {
+                Movie = await _omdbService.GetMovieByIdAsync(imdbId);
+                Reviews = await _database.Reviews.Where(r => r.ImdbID == imdbId).ToListAsync();
+                Review.ImdbID = imdbId;
+            }
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
+                // Hämta om filmen och recensionerna om det blev valideringsfel
+                Movie = await _omdbService.GetMovieByIdAsync(Review.ImdbID);
+                Reviews = await _database.Reviews.Where(r => r.ImdbID == Review.ImdbID).ToListAsync();
                 return Page();
             }
 
             Review.DateTime = DateTime.Now;
 
             _database.Reviews.Add(Review);
-
             await _database.SaveChangesAsync();
 
             return RedirectToPage("MovieDetailsPage", new { imdbId = Review.ImdbID });
