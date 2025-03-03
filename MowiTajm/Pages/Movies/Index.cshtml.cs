0e7a2ad1c90420c13a1642263a254ba92dd5ab59
audioken <kenzachrisson@gmail.com>
2025-03-03 20:42:38 +0100

    fixade hantering om man söker på något, men inte får någon träff. utan hantering krashade sidan när man sökte på typ "sdfasf" eller "sällskapsres"

--- MowiTajm/Pages/Movies/Index.cshtml.cs
+++ MowiTajm/Pages/Movies/Index.cshtml.cs
@@ -14,20 +14,25 @@ namespace MowiTajm.Pages.Movies
         }
 
         public List<MovieLite> Movies { get; set; } = new();
-        public string TotalResults { get; set; }
-        public string SearchInput { get; set; }
+        public string TotalResults { get; set; } = "";
+        public string SearchInput { get; set; } = "";
 
         public async Task OnGetAsync(string searchInput)
         {
             // Spara söksträngen för att kunna använda den i vyn
-            SearchInput = searchInput;
+            if (!string.IsNullOrWhiteSpace(searchInput))
+            {
+                SearchInput = searchInput;
+            }
 
             // Om söksträngen inte är tom, sök efter filmer
             if (!string.IsNullOrWhiteSpace(searchInput))
             {
                 // Anropa API:et för att söka efter filmer och hämta resultatet
                 var result = await _omdbService.SearchMoviesAsync(searchInput);
-                Movies = result.Search;
+
+                // Hämta filmer, men använd en tom lista om inga resultat hittades
+                Movies = result.Search ?? new List<MovieLite>();
 
                 // Spara totala antalet resultat för att kunna visa det i vyn
                 TotalResults = result.TotalResults;
