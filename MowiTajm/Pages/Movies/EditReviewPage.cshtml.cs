8e05c7c0d239f04228c11c6634236f154b320845
audioken <kenzachrisson@gmail.com>
2025-03-04 01:09:45 +0100

    ordnade med EditReviewPage och så man kan spara ändringar eller avbryta

--- MowiTajm/Pages/Movies/EditReviewPage.cshtml.cs
+++ MowiTajm/Pages/Movies/EditReviewPage.cshtml.cs
@@ -1,12 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MowiTajm.Data;
+using MowiTajm.Models;
 
 namespace MowiTajm.Pages.Movies
 {
     public class EditReviewPageModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _database;
+
+        public EditReviewPageModel(ApplicationDbContext database)
         {
+            _database = database;
+        }
+
+        [BindProperty]
+        public Review Review { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Review = await _database.Reviews.FindAsync(id);
+
+            if (Review == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Review.DateTime = DateTime.Now;
+
+            _database.Reviews.Update(Review);
+            await _database.SaveChangesAsync();
+
+            return RedirectToPage("MovieDetailsPage", new { imdbID = Review.ImdbID });
         }
     }
 }
