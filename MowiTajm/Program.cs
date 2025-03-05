958fec54961e8a6d10ccf5e011d5b4324a3f3f8c
Edrisa Jallow <edrisa.j98@hotmail.com>
2025-03-05 13:28:15 +0100

    ändrade alla IdentityUser till ApplicationUser

--- MowiTajm/Program.cs
+++ MowiTajm/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MowiTajm;
 using MowiTajm.Data;
+using MowiTajm.Models;
 using MowiTajm.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true) // --- Ändra till ApplicationUser från IdentityUser.
     .AddRoles<IdentityRole>() // <--- Lagt till AddRoles.
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
