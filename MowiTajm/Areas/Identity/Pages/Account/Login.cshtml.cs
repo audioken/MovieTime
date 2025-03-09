ed77046c37aba97fc0b491f8a948c3156f7f33dd
audioken <kenzachrisson@gmail.com>
2025-03-09 23:24:28 +0100

    stylade account-menyer: Login, Register, Forgot Password och ConfirmEmail

--- MowiTajm/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MowiTajm/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,19 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using MowiTajm.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MowiTajm.Areas.Identity.Pages.Account
 {
@@ -81,7 +74,7 @@ namespace MowiTajm.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Komihåg mig?")]
+            [Display(Name = "Kom ihåg mig?")]
             public bool RememberMe { get; set; }
         }
 
